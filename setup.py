30ba20861935c60632d858a59abb7ab5dd9f8afe
Chad Furman <chad@chadfurman.com>
2021-07-02 18:55:15 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Import needed function from setuptools
+from setuptools import setup, find_packages
+
+# Create proper setup to be used by pip
+setup(name='humble-bundle-api',
+      version='0.0.1',
+      description='Python interface to humble bundle library',
+      author='Lotus',
+      packages=find_packages(include=['pytest']),
+      install_requires=[
+          "pytest", "colorama"])
+
