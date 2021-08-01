0326f5a247dfcfba12ed1a971bb73f382f799423
Chad Furman <chad@chadfurman.com>
2021-08-01 16:54:28 -0400

    refactor: moved top level classes to src folder

--- src/credentials.py
+++ src/credentials.py
@@ -3,9 +3,9 @@ SIMPLEAUTH_SESS = None
 
 def get_cookies():
     if CSRF_COOKIE is None:
-        raise ValueError('CSRF_COOKIE not set in Credentials.py')
+        raise ValueError('CSRF_COOKIE not set in credentials.py')
     if SIMPLEAUTH_SESS is None:
-        raise ValueError('SIMPLEAUTH_SESS not set in Credentials.py')
+        raise ValueError('SIMPLEAUTH_SESS not set in credentials.py')
     return {
         'csrf_cookie': CSRF_COOKIE,
         '_simpleauth_sess': SIMPLEAUTH_SESS
