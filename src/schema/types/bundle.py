02d954388c2be4014707f4aa6b3bb2c83718225d
Chad Furman <chad@chadfurman.com>
2021-08-21 21:24:14 -0400

    chore: Flushed out Product schema (no resolvers) -- removed product_type because that's confusing.  Added TODOs where I don't know what fields do what.

--- src/schema/types/bundle.py
+++ src/schema/types/bundle.py
@@ -1,9 +1,10 @@
 from graphene import ObjectType, List, Field, String, Float, Date, Int, Boolean
+from .product import Product
 
 class Bundle(ObjectType):
     """The GraphQL Bundle type represents a Bundle the authenticated user has in their library"""
     amount_spent= Float(description="The amount of currency that the user has spent on this bundle, represented as a float e.x. 25.0")
-    product=Field(ProductType, description="The metadata associated with this Bundle which HumbleBundle calls a Product")
+    product=Field(Product, description="The metadata associated with this Bundle which HumbleBundle calls a Product")
     gamekey= String(description="A gamekey, if any, for this bundle.  Not all bundles may have a gamekey.  ex: '2AZm2qDRsVUeqbKY'")
     uid= String(description="A unique identifier for this Bundle used for several purposes in the HumbleBundle API.  Ex: 'X9GHPP9ABKS28'")
     all_coupon_data= List(Field(CouponType), description="A list of coupons which came with this bundle.")
--- src/schema/types/product.py
+++ src/schema/types/product.py
@@ -1,1 +1,12 @@
-{"category": "bundle", "machine_name": "2danimation_softwarebundle", "empty_tpkds": {}, "post_purchase_text": "", "human_name": "Humble Software Bundle: 2D Animation", "partial_gift_enabled": true}
+from graphene import ObjectType, String, Field, Boolean
+
+#from .tpkd import EmptyTpkds
+
+class Product(ObjectType):
+    """A product that has been purchased on the Humble Bundle website.  Could be a Bundle, could be something else."""
+    category=String(description="The category of product, for example 'bundle'.  TODO: Make this an enum with known categories")
+    machine_name= String(descrption="The name that the Humble Bundle system calls this product, for exampel '2danimation_softwarebundle'")
+   # empty_tpkds= Field(EmptyTpkds, description="TODO: What is this field?")
+    post_purchase_text= String(description="TODO: What is this?  I assume it's text that displays on the screen after a bundle is purchased and likely is not important for our purposes?")
+    human_name= String(description="The human-readable name of this bundle.  For example: 'Humble Software Bundle: 2D Animation'")
+    partial_gift_enabled= Boolean(description="TODO: What's the purpose of this field?")
